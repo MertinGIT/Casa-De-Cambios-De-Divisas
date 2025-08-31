af711f54a4089ff7bf7ba4a5ee1c9d844dd76f9d
Alanalcarazpy7 <alanalcaraz010@gmail.com>
2025-08-31 12:46:55 -0300

    solucionando bug moneda y cotizacion

--- global_exchange/cotizaciones/urls.py
+++ global_exchange/cotizaciones/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", views.cotizacion_lista, name="cotizacion"),
     path("nuevo/", views.cotizacion_nuevo, name="cotizacion_nuevo"),
     path("editar/<int:pk>/", views.cotizacion_editar, name="cotizacion_editar"),
-    path("eliminar/<int:pk>/", views.cotizacion_eliminar, name="cotizacion_eliminar"),
+    path("eliminar/<int:pk>/", views.cotizacion_desactivar, name="cotizacion_desactivar"),
     path('cotizaciones/detalle/<int:pk>/', views.cotizacion_detalle, name='cotizacion_detalle'),
 ]
