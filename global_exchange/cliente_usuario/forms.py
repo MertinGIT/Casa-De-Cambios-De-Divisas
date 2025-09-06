dd9f1cf32d8cdaf2c02097e89540cfe7cc52ef7f
ruben.lopez <ruben.lopez@databiz.com.py>
2025-09-05 21:25:02 -0300

    Documentación Cliente-Usuario

--- global_exchange/cliente_usuario/forms.py
+++ global_exchange/cliente_usuario/forms.py
@@ -1,4 +1,3 @@
-# clientes/forms.py
 from django import forms
 from django.contrib.admin.widgets import FilteredSelectMultiple
 from clientes.models import Cliente
@@ -7,9 +6,30 @@ from .models import Usuario_Cliente
 
 class ClienteUsuariosForm(forms.ModelForm):
     """
-    Formulario para asignar usuarios a un cliente.
-    """
+    Formulario para asignar usuarios a un cliente específico.
 
+    Campos
+    ------
+    usuarios : ModelMultipleChoiceField
+        Lista de usuarios disponibles para asignar al cliente.  
+        Se utiliza el widget ``FilteredSelectMultiple`` para selección múltiple con estilo de admin de Django.
+
+    Métodos
+    -------
+    __init__(*args, **kwargs)
+        Inicializa el formulario y precarga los usuarios asignados si el cliente ya existe.
+
+    save(commit=True)
+        Guarda los cambios del cliente y actualiza la tabla intermedia ``Usuario_Cliente``:
+            - Elimina las relaciones anteriores del cliente.
+            - Crea nuevas relaciones para los usuarios seleccionados.
+
+    Notas
+    -----
+    - El formulario está vinculado al modelo ``Cliente`` y solo expone el campo ``usuarios``.
+    - Las relaciones intermedias se manejan de forma automática al guardar el formulario.
+    """
+    
     usuarios = forms.ModelMultipleChoiceField(
         queryset=CustomUser.objects.all(),
         widget=FilteredSelectMultiple("Usuarios asignados", is_stacked=False),
@@ -22,12 +42,25 @@ class ClienteUsuariosForm(forms.ModelForm):
         fields = ['usuarios']
 
     def __init__(self, *args, **kwargs):
+        """
+        Inicializa el formulario y precarga los usuarios asignados si el cliente ya existe.
+
+        :param args: Argumentos posicionales para el constructor del formulario.
+        :param kwargs: Argumentos de palabra clave para el constructor del formulario.
+        """
         super().__init__(*args, **kwargs)
-        # Precargar usuarios asignados si el cliente ya existe
         if self.instance and self.instance.pk:
             self.fields['usuarios'].initial = self.instance.usuarios.all()
 
     def save(self, commit=True):
+        """
+        Guarda los cambios del cliente y actualiza las relaciones intermedias con los usuarios.
+
+        :param commit: Indica si se debe guardar inmediatamente en la base de datos.
+        :type commit: bool
+        :return: Instancia del cliente guardada.
+        :rtype: Cliente
+        """
         cliente = super().save(commit=False)
         if commit:
             cliente.save()
---
+++ global_exchange/docs/source/cliente_usuario.rst
@@ -0,0 +1,24 @@
+Cliente - Usuario
+=================
+
+Esta aplicación gestiona la relación entre usuarios y clientes en el sistema.
+
+Modelos
+-------
+.. autoclass:: cliente_usuario.models.Usuario_Cliente
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Formularios
+-----------
+.. autoclass:: cliente_usuario.forms.ClienteUsuariosForm
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Vistas
+------
+.. autofunction:: cliente_usuario.views.cliente_usuarios_lista
+.. autofunction:: cliente_usuario.views.editar_cliente_usuario
+.. autofunction:: cliente_usuario.views.cliente_usuario_detalle
