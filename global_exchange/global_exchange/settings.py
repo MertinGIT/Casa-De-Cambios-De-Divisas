c021e7eb82fc241840bbfbe3310f2cf744baa342
VarLuks <florentinlucas77@gmail.com>
2025-09-24 22:57:25 -0300

    Primera version de los historiales, falta agregar al sidebar y arreglar css

--- global_exchange/global_exchange/settings.py
+++ global_exchange/global_exchange/settings.py
@@ -94,6 +94,7 @@ INSTALLED_APPS = [
     'cliente_usuario',
     'operaciones',
     'corsheaders',
+    'historial_transacciones'
 ]
 # ============================================================================
 # Middleware
